75120cee87409ff56283dc8bae51d4fbc17a0dac
Rajan <rajanj448@student.sfit.ac.in>
2024-04-17 17:37:10 +0530

    initial

---
+++ backend/src/main.ts
@@ -0,0 +1,48 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import helmet from 'helmet';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+
+  app.enableCors();
+  app.use(helmet());
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Organise Simple')
+    .setContact(
+      'Rajan Jha',
+      'https://github.com/CodingKing2003',
+      'rajanj448@gmail.com',
+    )
+    .setDescription(
+      'Organize Simple is an API that allows you to organize your data in a way that is easy to use and understand with the power of large language models.',
+    )
+    .setVersion('1.0')
+    .addApiKey(
+      {
+        type: 'apiKey',
+        name: 'X-API-KEY',
+        in: 'header',
+        description: 'API key for authentication of registered applications',
+      },
+      'apiKey',
+    )
+    .addTag('organize-simple')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen(3000);
+}
+bootstrap();
